c5a01e53a18d088a9f3d87cc57fc97637e42b9a2
Hal Fairless and Nigel Burnett <nigel@>
2016-10-18 10:12:54 -0700

    Add images and jquery to toggle both images

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $(".clickable1").click(function() {
+    $("#walrus-showing").toggle();
+    $("#walrus-hidden").toggle();
+  });
+
+  $(".clickable2").click(function() {
+    $("#walrus-showing2").toggle();
+    $("#walrus-hidden2").toggle();
+  });
+
+
+});
